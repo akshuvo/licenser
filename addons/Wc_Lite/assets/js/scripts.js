40c47ae2549d12260f91f5713cf235012e91d923
Akhtarujjaman Shuvo <aktarujjman@gmail.com>
2024-01-24 03:49:36 +0600

    clean

--- addons/Wc_Lite/assets/js/scripts.js
+++ addons/Wc_Lite/assets/js/scripts.js
@@ -1,19 +1,14 @@
-(function($) {
-	"use strict";
-	$(document).ready(function(){
-		
-		$(document).on('click', '.show_manage_activations_details a', function(e){
-            e.preventDefault();
-            jQuery('.manage-activations').removeClass('active');
-            $(this).closest('td').find('.manage-activations').toggleClass('active');
-        });
+jQuery(document).ready(function(){
 
-        $(document).on('click', '.manage-activations .activations-close-modal', function(e){
-            e.preventDefault();
-            $(this).closest('td').find('.manage-activations').removeClass("active");
-        });
+    jQuery(document).on('click', '.show_manage_activations_details a', function(e){
+        e.preventDefault();
+        jQuery('.manage-activations').removeClass('active');
+        jQuery(this).closest('td').find('.manage-activations').toggleClass('active');
+    });
 
+    jQuery(document).on('click', '.manage-activations .activations-close-modal', function(e){
+        e.preventDefault();
+        jQuery(this).closest('td').find('.manage-activations').removeClass("active");
+    });
 
-	});
-
-})(jQuery);
+});
--- addons/Wc_Lite/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-//add_action( 'woocommerce_thankyou', 'wc_generate_license', 15 );
-add_action( 'woocommerce_order_status_changed', 'wc_generate_license', 150 );
-//add_action( 'woocommerce_checkout_update_order_meta', 'wc_generate_license', 15 );
